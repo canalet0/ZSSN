884d227b64d0fab501fb8f7d79502eb8ee3cb771
canaleto <roc0canal@gmail.com>
2020-09-07 16:35:11 -0300

    Init Spring Boot project

---
+++ src/test/java/br/com/zssn/ZssnApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zssn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZssnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
